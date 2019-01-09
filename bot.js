3af4da5bf6fad0762208864ec45a940fb13a2e14
zaykha2 <45631038+zaykha2@users.noreply.github.com>
2019-01-09 07:52:49 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on("ready", async() => {
 console.log(`Back Online In ${client.guilds.size} Servers!`);
 console.log(`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=8\nInvite Me To Your Server!`);
     setTimeout(() => {
-        client.user.setActivity(`${prefix}help | V 1.1`, {type: "WATCHING"});
+        client.user.setActivity(`${prefix} Madona ,`, {type: "WATCHING"});
     }, 3000);
 });
 
